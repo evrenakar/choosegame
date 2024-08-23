ff67fd45a8056442ffb1b1b0b191f013832e5a47
evrenakar <ben@evrenakar.com>
2024-08-23 21:28:07 +0300

    Fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['evrenakar.github.io'],
+        domains: ['https://evrenakar.github.io/**'],
       },
 };
 
