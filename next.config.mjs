8dfeac467aa9c97d11b25d84fa9c62fe094bee4d
evrenakar <ben@evrenakar.com>
2024-08-23 21:24:56 +0300

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [
+        domains: [
           {
             protocol: 'https',
             hostname: '**.github.io',
