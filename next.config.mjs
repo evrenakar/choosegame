8eaa09bd8a670b1d3a694e2befe3d6ea9ed1308f
evrenakar <ben@evrenakar.com>
2024-08-23 21:21:39 +0300

    Fix

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
         remotePatterns: [
           {
             protocol: 'https',
-            hostname: 'evrenakar.github.io',
+            hostname: '**.github.io',
             port: '',
           },
         ],
